333819cba4df063340bafc2f79ff669c9338a76b
SimoneGrant <simonegrant001@gmail.com>
2018-06-19 09:26:35 -0400

    add file

---
+++ Intro_To_iOS.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Print Hello World.
+
+//PART I - Storing Values
+
+let name: String = "John"
+let greeting: String = "Welcome to Inclusion "
+let message: String = greeting + name
+//print the message
+
+
+//PART II - IF Statement
+
+var timeOfDay = "night"
+
+
+//PART III - Array
+
+var menu = ["Home", "About", "Contact"]
+
+//Loop through the array to print out each item
+
+//Get a specific item from menu
+
+
+//PART VI - Solving Complex Problems
+
+let price: Int = 10
+let amountPurchased: Int = 3
+//What is the total price? How would you solve it?
+
+
+
+
+//PART V - Have fun!
+
+let swiftBlockLetters = "   _____         _  __ _​  \n  / ____|       (_)/ _| |   \n | (_____      ___| |_| |_  \n  \\___ \\ \\ /\\ / / |  _| __| \n  ____) \\ V  V /| | | | |_  \n |_____/ \\_/\\_/ |_|_|  \\__|"
+
+ let swiftBird = " |                            -                   \n |                            .ds-                \n |              :.             `mMh-              \n |         -+.   /y:            :MMMy`            \n |          `sd/` .dd/           hMMMN:           \n |            -dNy- +MNs.        +MMMMM/          \n |              +NMmo:hMMh/`     /MMMMMM.         \n |               `oNMMdyNMMNy:   oMMMMMMh         \n |                 `oNMMMMMMMMNs:mMMMMMMM         \n |                   `oNMMMMMMMMMMMMMMMMM`        \n |     `                +mMMMMMMMMMMMMMMN         \n |      +y/.              :dMMMMMMMMMMMMy         \n |       .hMNhs+/:----:/oydMMMMMMMMMMMMMMh`       \n |         -yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh       \n |           `+hMMMMMMMMMMMMMMMMMMMMdhhdNMM/      \n |              `:oymNMMMMMMMMMNho-      :hs      \n |                    `.-:::-.`            `      \n |"
+
+
+
